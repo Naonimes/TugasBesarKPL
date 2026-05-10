37a11312e8309201bfb47c1632b991d750c492e9
Shidiq <shidiqbachtiar05@gmail.com>
2026-05-10 21:29:20 +0700

    Menambahkan MenuModule dan unit testing

---
+++ TugasBesarKPL.Tests/MenuModuleTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TugasBesarKPL_Solution;
+
+namespace TugasBesarKPL.Tests
+{
+    [TestClass]
+    public class MenuModuleTests
+    {
+        [TestMethod]
+        public void GetCategoryCode_ShouldReturnCorrectCode_WhenCategoryIsMakanan()
+        {
+            var menu = new MenuModule();
+
+            string code = menu.GetCategoryCode("Makanan");
+
+            Assert.AreEqual("MKN-01", code);
+        }
+
+        [TestMethod]
+        public void GetCategoryCode_ShouldReturnUnknown_WhenCategoryNotFound()
+        {
+            var menu = new MenuModule();
+
+            string code = menu.GetCategoryCode("Pizza");
+
+            Assert.AreEqual("UNKNOWN", code);
+        }
+
+        [TestMethod]
+        public void GetCategoryCode_ShouldThrowException_WhenCategoryEmpty()
+        {
+            var menu = new MenuModule();
+
+            try
+            {
+                menu.GetCategoryCode("");
+                Assert.Fail("Seharusnya terjadi ArgumentException.");
+            }
+            catch (ArgumentException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        public void AddData_ShouldIncreaseCount_WhenDataIsValid()
+        {
+            var storage = new MenuModule.DataStorage<string>();
+
+            storage.AddData("Nasi Goreng");
+
+            Assert.AreEqual(1, storage.Count);
+        }
+
+        [TestMethod]
+        public void AddData_ShouldThrowException_WhenDataIsNull()
+        {
+            var storage = new MenuModule.DataStorage<string>();
+
+            try
+            {
+                storage.AddData(null);
+                Assert.Fail("Seharusnya terjadi ArgumentNullException.");
+            }
+            catch (ArgumentNullException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
--- TugasBesarKPL_Solution/Program.cs
+++ TugasBesarKPL_Solution/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TugasBesarKPL_Solution;
 
 namespace TugasBesarKPL
 {
